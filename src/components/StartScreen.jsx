ee3d61a3be8d006f9c8cc1523fa934bec79f73eb
Aaron <115331438+Aaron2578@users.noreply.github.com>
2026-06-05 10:36:47 +0530

    Update StartScreen.jsx

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,8 +1,8 @@
 function StartScreen({ numQuestions, dispatch }) {
     return (
         <div className="start">
-            <h2>Welcome to The Python Quiz!</h2>
-            <h3>{numQuestions} Question to test your React Mastery</h3>
+            <h2>Welcome to The SAP FICO Quiz!</h2>
+            <h3>{numQuestions} Question to test your SAP FICO Mastery</h3>
             <button className="btn btn-ui" onClick={() => dispatch({ type: 'start' })}>Let's Start</button>
         </div>
     )
